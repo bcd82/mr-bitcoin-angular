da483b839a8e067c0045bfa67c43a7c0d8a7bfce
bcd82 <barak.sidi@gmail.com>
2021-10-31 23:09:21 +0200

    router , contacts , contact details and charts page are here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppHeaderComponent } from './cmps/app-header/app-header.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ChartsComponent } from './pages/charts/charts.component';
+import { ContactPreviewComponent } from './cmps/contact-preview/contact-preview.component';
+import { ContactListComponent } from './cmps/contact-list/contact-list.component';
+import { ContactDetailsComponent } from './pages/contact-details/contact-details.component';
+import { ContactFilterComponent } from './cmps/contact-filter/contact-filter.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
 
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'contacts/:id', component: ContactDetailsComponent },
+  { path: 'charts', component: ChartsComponent },
+
+  // { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, AppHeaderComponent, ContactsComponent, ChartsComponent, ContactPreviewComponent, ContactListComponent, ContactDetailsComponent, ContactFilterComponent],
+  imports: [RouterModule.forRoot(appRoutes), BrowserModule,  FormsModule,HttpClientModule,ChartsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
