0fe8fb1e3550405e7d1d696e453e1f643a81b501
bcd82 <barak.sidi@gmail.com>
2021-11-03 16:33:12 +0200

    Auto-generated commit [ci skip]

--- src/app/pipes/natural-typing.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NaturalTypingPipe } from './natural-typing.pipe';
-
-describe('NaturalTypingPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NaturalTypingPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
